6575068eeef6ae74c659dece4ba21f69de6a492b
Cillianbc <cillianbc@gmail.com>
2015-01-03 12:38:06 +1100

    Added small Header Text

--- js/app.js
+++ js/app.js
@@ -1,36 +1,63 @@
-$(document).ready(function() {
-  $('.ryu').mouseenter(function() {
-    $('.ryu-still').hide();
-    $('.ryu-ready').show();
+$(document).ready(function () {
+  $('.ryu').mouseenter(function () {
+    Ready();
   })
-  .mouseleave(function() {
+    .mouseleave(function () {
+      basePosition();
+    })
+    .mousedown(function () {
+      playHadouken();
+      hadouken();
+    })
+    .mouseup(function () {
+      HadoukenStop();
+    })
+})
+
+//Function Roll Up //
+  function Ready(){
+  $('.ryu-still').hide();
+  $('.ryu-ready').show();
+  }
+  function basePosition(){
     $('.ryu-ready').hide();
-    $('.ryu-still').show();
-  })
-  .mousedown(function() {
-    playHadouken();    
+      $('.ryu-still').show();
+  }
+  function playHadouken() {
+    $('#hadouken-sound')[0].volume = 0.5;
+    $('#hadouken-sound')[0].load();
+    $('#hadouken-sound')[0].play();
+  }
+function HadoukenStop(){
+      $('.ryu-throwing').hide();
+      $('.ryu-ready').show();
+}
+
+  function hadouken() {
+    playHadouken();
     $('.ryu-ready').hide();
     $('.ryu-throwing').show();
     $('.hadouken').finish().show()
-    .animate(
-      {'left': '300px'},
-      500,
-      function() {
+      .animate({
+        'left': '300px'
+      }, 500, function () {
         $(this).hide();
         $(this).css('left', '-212px');
-      }
-    );
+      });
+  }
+
+//Function Roll Up//
+
+
+  $(document).keypress(function (e) {
+    if (event.keyCode == 120) {
+      Ready();
+      playHadouken();
+      hadouken();
+    }
   })
-  .mouseup(function() {
-    $('.ryu-throwing').hide();
-    $('.ryu-ready').show();
-  });
-});
-
-function playHadouken () {
-  $('#hadouken-sound')[0].volume = 0.5;
-  $('#hadouken-sound')[0].load();
-  $('#hadouken-sound')[0].play();
-}
-
-$('.ryu').keydown(event.which==88)
+  $(document).keyup(function (e) {
+    if (event.keyCode == 120) {
+      HadoukenStop();
+    };
+  })
